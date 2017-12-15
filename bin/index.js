8ec92d0477deda3b12a8bc0e37ad0a46922bf089
xudeming208 <xudeming208@126.com>
2017-12-15 20:14:53 +0800

    update

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-if (parseFloat(process.versions.node) < 0.6) {
-	return console.warn('Node 版本过低, 请升级至最新版本.');
+if (parseFloat(process.versions.node) < 7.6) {
+	return console.warn('mid框架采用了async/await，node版本需要>=7.6.0才支持，请更新node版本至>=7.6.0!');
 }
 
 require('../lib/init');
